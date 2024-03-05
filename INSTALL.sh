6c3b43951b91158121b6fdc93664921c38763649
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-03-05 10:26:32 +0530

    Resolve on julia version 1.10.2 (#41)
    * Resolve on julia version 1.10.2
    
    * Support version as a positional argument for backward compatibility
    
    * Use exit keyword
    
    * Use different exit codes in install test script
    
    * Fix passing cmd line args
    
    * ensure that positional arguments follow opts

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,26 @@
 #! /bin/bash
 set -euo pipefail
 
-curl -fsSL https://install.julialang.org | sh -s -- --yes
+args=$@
 source ./juliaversion
-juliaversion=${1:-$RossbyWaveJuliaVersion}
-bash --rcfile ~/.bashrc -i ./installjulia.sh $juliaversion
+juliaversion=$RossbyWaveJuliaVersion
+VERSIONFLAG=false
+while getopts ":rv:" flag; do
+    case ${flag} in
+        v)
+            VERSIONFLAG=true
+            juliaversion=$OPTARG
+            ;;
+    esac
+done
+# backward compatibility with version as a positional argument
+if [ $VERSIONFLAG == false ]; then
+    shift $((OPTIND - 1))
+    juliaversion=${1:-$juliaversion}
+fi
+echo "using Julia version $juliaversion"
+curl -fsSL https://install.julialang.org | sh -s -- --yes --default-channel=$juliaversion
+bash --rcfile ~/.bashrc -i ./installjulia.sh $args
 if [ $? == 0 ]; then
 	echo "Installation successful."
 	echo "You may want to either source your shell's rc script, or launch a new interactive shell."
--- installjulia.sh
+++ installjulia.sh
@@ -1,8 +1,42 @@
 source ./juliaversion
-juliaversion=${1:-$RossbyWaveJuliaVersion}
+juliaversion=$RossbyWaveJuliaVersion
+RESOLVE=false
+VERSIONFLAG=false
+while getopts ":rv:" flag; do
+    case ${flag} in
+        r)
+        	RESOLVE=true
+            ;;
+        v)
+			VERSIONFLAG=true
+            juliaversion=$OPTARG
+            ;;
+        ?)
+            echo "Invalid option: -${OPTARG}"
+            exit 1
+            ;;
+    esac
+done
+# backward compatibility with version as a positional argument
+if [ $VERSIONFLAG == false ]; then
+	shift $((OPTIND - 1))
+	juliaversion=${1:-$juliaversion}
+fi
+
 juliaup add $juliaversion
-julia +$juliaversion -e \
-'import Pkg;
-Pkg.activate("ApproxFunAssociatedLegendre"); Pkg.instantiate();
-Pkg.activate("."); Pkg.instantiate();
-Pkg.activate("RossbyPlots"); Pkg.instantiate();'
+if [ $RESOLVE == true ]; then
+	echo "Resolving environments on Julia version $juliaversion"
+	julia +$juliaversion -e \
+	'import Pkg;
+	Pkg.activate("ApproxFunAssociatedLegendre"); Pkg.resolve(); Pkg.instantiate();
+	Pkg.activate("."); Pkg.resolve(); Pkg.instantiate();
+	Pkg.activate("RossbyPlots"); Pkg.resolve(); Pkg.instantiate();
+	'
+else
+	julia +$juliaversion -e \
+	'import Pkg;
+	Pkg.activate("ApproxFunAssociatedLegendre"); Pkg.instantiate();
+	Pkg.activate("."); Pkg.instantiate();
+	Pkg.activate("RossbyPlots"); Pkg.instantiate();
+	'
+fi
